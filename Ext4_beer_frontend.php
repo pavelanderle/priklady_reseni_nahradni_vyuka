efccf7fa40622d23ec301dc917029f6a66b3f286
Pavel Anderle <anderle@spseplzen.cz>
2020-03-28 18:33:12 +0100

    add beer frondend

---
+++ Ext4_beer_frontend.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="cz">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <title>BEER - účtenka</title>
+</head>
+<body>
+    <div class="container p-3 my-3 border">
+    <h2>PIVNÍ ÚČTENKA</h2>
+    <div class="container p-3 my-3 border">
+    <form action="#" method="get">
+        <div class="form-group">
+            <label for="NameBeer">Název piva:</label>
+            <select class="form-control" id="nameBeer" name="nameBeer">
+              <option value="Prazdroj">Prazdroj</option>
+              <option value="Gambrinus">Gambrinus</option>
+              <option value="Purkmistr">Purkmistr</option>
+            </select>
+        </div>
+        <div class="form-check-inline">
+            <label class="form-check-label">
+              <input type="radio" class="form-check-input" name="typeBeer" value="cp">Čepované pivo
+            </label>
+          </div>
+        <div class="form-check-inline">
+            <label class="form-check-label">
+              <input type="radio" class="form-check-input" name="typeBeer" value="lp">Lahvové pivo
+            </label>
+        </div>
+        <div class="form-group">
+            <label for="usr">Počet piv:</label>
+            <input type="number" class="form-control" id="numBeer" name="numBeer">
+          </div>
+        <div class="form-group">
+            <input type="submit" value="Cena piv">
+        </div>
+    </form>
+    </div>
+    <div class="container p-3 my-3 border" >
+      <h2>Souhrn:</h2>
+      <?php
+        if (isset($_GET["nameBeer"])) {
+          include "Ext4_beer.php";
+          $priceBeer = array("Prazdroj"=>23,"Gambrinus"=>18,"Purkmistr"=>20);
+          $priceMyBeer = $priceBeer[$_GET["nameBeer"]];
+          if ($_GET["typeBeer"]=="cp") {
+            $myBeer = new BeerDraft($_GET["nameBeer"],$priceMyBeer,$_GET["numBeer"]);
+          }
+          else {
+            $myBeer = new BeerBootled($_GET["nameBeer"],$priceMyBeer,$_GET["numBeer"]);
+          }
+          
+          echo "<p><strong>Zadané pivo: </strong>". $myBeer => info()."</p>";
+          echo "<p><strong>Celková cena: </strong>". $myBeer=>getPriceSum()."</p>";
+        } 
+      ?>
+    </div>
+    </div>
+</body>
+</html>
