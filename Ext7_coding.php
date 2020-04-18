dbdcafbf4eeca9548e6a8f470d06b10dcbd23abd
Pavel Anderle <anderle@spseplzen.cz>
2020-04-18 14:57:33 +0200

    complete

--- Ext7_coding.php
+++ Ext7_coding.php
@@ -2,7 +2,7 @@
 /* Třída - rodič pro potomky, představující třídy pro kódování a dekodování textu */
 abstract class codingString{
     protected $nameCoding; // název Kódování např. "Konverze text do Morzeovy abecedy"
-    
+
     public function __construct($nameC){
         $this->nameCoding = $nameC;
     }
@@ -25,40 +25,40 @@ abstract class codingString{
 class Morse extends CodingString{
     const MORSECODE = array(
         "a"=>".-",
-        "b"=>"-...", 
-        "c"=>"-.-.", 
-        "d"=>"-..", 
-        "e"=>".", 
-        "f"=>"..-.", 
-        "g"=>"--.", 
-        "h"=>"....", 
-        "i"=>"..", 
-        "j"=>".---", 
-        "k"=>"-.-", 
-        "l"=>".-..", 
-        "m"=>"--", 
-        "n"=>"-.", 
-        "o"=>"---", 
-        "p"=>".--.", 
-        "q"=>"--.-", 
-        "r"=>".-.", 
-        "s"=>"...", 
-        "t"=>"-", 
-        "u"=>"..-", 
-        "v"=>"...-", 
-        "w"=>".--", 
-        "x"=>"-..-", 
-        "y"=>"-.--", 
-        "z"=>"--..", 
+        "b"=>"-...",
+        "c"=>"-.-.",
+        "d"=>"-..",
+        "e"=>".",
+        "f"=>"..-.",
+        "g"=>"--.",
+        "h"=>"....",
+        "i"=>"..",
+        "j"=>".---",
+        "k"=>"-.-",
+        "l"=>".-..",
+        "m"=>"--",
+        "n"=>"-.",
+        "o"=>"---",
+        "p"=>".--.",
+        "q"=>"--.-",
+        "r"=>".-.",
+        "s"=>"...",
+        "t"=>"-",
+        "u"=>"..-",
+        "v"=>"...-",
+        "w"=>".--",
+        "x"=>"-..-",
+        "y"=>"-.--",
+        "z"=>"--..",
         "0"=>"-----",
-        "1"=>".----", 
-        "2"=>"..---", 
-        "3"=>"...--", 
-        "4"=>"....-", 
-        "5"=>".....", 
-        "6"=>"-....", 
-        "7"=>"--...", 
-        "8"=>"---..", 
+        "1"=>".----",
+        "2"=>"..---",
+        "3"=>"...--",
+        "4"=>"....-",
+        "5"=>".....",
+        "6"=>"-....",
+        "7"=>"--...",
+        "8"=>"---..",
         "9"=>"----.",
         "."=>".-.-.-",
         ","=>"--..--",
@@ -70,9 +70,9 @@ class Morse extends CodingString{
         $string_lower = strtolower($text);
         $code = "";
         for($i=0;$i<strlen($string_lower);$i++){
-            $index = $string_lower[$i];       
+            $index = $string_lower[$i];
             $code .= SELF::MORSECODE["$index"]." ";
-               
+
         }
         return $code;
     }
@@ -98,5 +98,5 @@ class Morse extends CodingString{
 $morsecoding = new Morse("Kódování morseovka");
 echo "<h2>". $morsecoding->info()."</h2>";
 echo $morsecoding -> coding("Ahoj");
-echo $morsecoding -> decoding(".-|....|---|.---");
+echo $morsecoding -> decoding("-|---|-.--|---|-|.-|-.-.|---|.-.|---|.-..|.-");
 ?>
